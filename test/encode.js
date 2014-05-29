9fa0a16e58805994fb3f263ec4aea19944a3355f
Mathias Buus <mathiasbuus@gmail.com>
2014-05-29 22:21:13 +0200

    added tests

---
+++ test/encode.js
@@ -0,0 +1,64 @@
+var tape = require('tape')
+var concat = require('concat-stream')
+var protocol = require('../')
+
+tape('meta', function(t) {
+  var p = protocol()
+
+  p.pipe(concat(function(data) {
+    t.same(data[0], 0, 'id')
+    t.same(data[1], 12, 'length')
+    t.same(data.slice(2).toString(), '{"change":1}', 'payload')
+    t.end()
+  }))
+
+  p.meta({change:1})
+  p.finalize()
+})
+
+tape('document', function(t) {
+  var p = protocol()
+
+  p.pipe(concat(function(data) {
+    t.same(data[0], 1, 'id')
+    t.same(data[1], 17, 'length')
+    t.same(data.slice(2).toString(), '{"hello":"world"}', 'payload')
+    t.end()
+  }))
+
+  p.document({hello:'world'})
+  p.finalize()
+})
+
+tape('protobuf', function(t) {
+  var p = protocol()
+
+  p.pipe(concat(function(data) {
+    t.same(data[0], 2, 'id')
+    t.same(data[1], 18, 'length')
+    t.same(data.slice(2).toString(), 'should be protobuf', 'payload')
+    t.end()
+  }))
+
+  p.protobuf(new Buffer('should be protobuf'))
+  p.finalize()
+})
+
+tape('blob', function(t) {
+  var p = protocol()
+
+  p.pipe(concat(function(data) {
+    t.same(data[0], 3, 'id')
+    t.same(data[1], 11, 'length')
+    t.same(data.slice(2).toString(), 'hello world', 'payload')
+    t.end()
+  }))
+
+  var bl = p.blob(11)
+
+  bl.write('hello')
+  bl.write(' ')
+  bl.write('world')
+
+  p.finalize()
+})
